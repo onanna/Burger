25f1d98039b60eecbdbe7ef9b92fd6296b143c95
Bawo Nanna <nannaofe20@gmail.com>
2019-02-03 14:53:49 -0500

    MySQL connection established

--- config/connection.js
+++ config/connection.js
@@ -1,2 +1,25 @@
 // set up MySQL connection
-var 
+var mysql = require("mysql");
+
+//create connection to mysql
+var connection = mysql.createConnection({
+    port: 3306,
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "burgers_db"
+});
+
+// make connection
+
+connection.connect(function(err) {
+    if (err){
+        console.error("error connecting " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+
+});
+
+// export connection for our ORM to use
+module.exports = connection ;
